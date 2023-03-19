d324af70353a355da4a24195af9ca4083b040837
Erlicer <95727981+Erlicer@users.noreply.github.com>
2023-03-19 04:50:45 -0500

    Update MusicBeatState.hx

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -118,6 +118,9 @@ class MusicBeatState extends FlxUIState
 		var skip:Bool = FlxTransitionableState.skipNextTransOut;
 		super.create();
 
+		if(!skip) {
+			openSubState(new CustomFadeTransition(0.7, true));
+		}
 		FlxTransitionableState.skipNextTransOut = false;
 	}
 
@@ -197,6 +200,24 @@ class MusicBeatState extends FlxUIState
 	}
 
 	public static function switchState(nextState:FlxState) {
+		// Custom made Trans in
+		var curState:Dynamic = FlxG.state;
+		var leState:MusicBeatState = curState;
+		if(!FlxTransitionableState.skipNextTransIn) {
+			leState.openSubState(new CustomFadeTransition(0.6, false));
+			if(nextState == FlxG.state) {
+				CustomFadeTransition.finishCallback = function() {
+					FlxG.resetState();
+				};
+				//trace('resetted');
+			} else {
+				CustomFadeTransition.finishCallback = function() {
+					FlxG.switchState(nextState);
+				};
+				//trace('changed state');
+			}
+			return;
+		}
 		FlxTransitionableState.skipNextTransIn = false;
 		FlxG.switchState(nextState);
 	}
