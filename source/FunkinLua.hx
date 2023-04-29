975fc990158d9703ce6ac552e96d42dc0d399d4f
Erlicer <95727981+Erlicer@users.noreply.github.com>
2023-04-28 20:07:03 -0500

    cego do krl

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -259,14 +259,14 @@ class FunkinLua {
 			var me = this;
 			@:privateAccess
 			var achieveID:Int = Achievements.getAchievementIndex(name);
-			if(!Achievements.isAchievementUnlocked(Achievements.achievementsStuff[achieveID][2]))
+			if(!Achievements.isAchievementUnlocked(Achievements.achievementsStuff[achieveID][2])) {
 				Achievements.unlockAchievement(name);
 				add(new AchievementObject(name, camAchievement));
 				ClientPrefs.saveSettings();
 				FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
 				return true;
 			}
-			else{return false;}
+			else return false;
 		});
 
 		// shader shit
