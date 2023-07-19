cd3dd2db9467041bbde769d58c45aba8ffe6de69
Erlicer <95727981+Erlicer@users.noreply.github.com>
2023-07-19 00:14:14 -0500

    Create GJKeys.hx

---
+++ source/GJKeys.hx
@@ -0,0 +1,6 @@
+package;
+class GJKeys
+{
+    public static var id:Int = 	746342; // Put your game's ID here
+    public static var key:String = "33b8a4901e00846ae1c2398c8c0f8985"; // Put your game's private API key here
+}
